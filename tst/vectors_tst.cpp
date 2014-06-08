626b21d8361e7819f37bc3cacc153c526862a4cf
peter <peter@peter-vb-linux.(none)>
2014-06-08 20:33:38 +0400

    Everyting fucked-up after installing virtualbox guest-additions ... seeking ways to fix...

--- tst/vectors_tst.cpp
+++ tst/vectors_tst.cpp
@@ -1,13 +1,21 @@
 
 #include <stdio.h>
 
+#define DBG_SIMPLE_VECTORS
 #include <utils/simple_vectors.h>
+
 
 int main(int argc, char* argv[])
 {
-
-	Vec2<int>::data_type a;
-
+	Vec2_dbg<int>::data_type a;
+	Vec2_dbg<float> b(.0f, 2.0f);
+	printf("Vector B:"); b.TRACE();
+	Vec2_dbg<float> c(3.0f, 0.f);
+	printf("Vector C:"); c.TRACE();
+	(b + c).TRACE();
+	(b - c).TRACE();
+	((b + c).normalize()).TRACE();
+	((b - c).normalize()).TRACE();
 	printf("All done, press ENTER to exit ...");
 	getchar();
 	return 0;
--- utils/simple_vectors.h
+++ utils/simple_vectors.h
@@ -2,16 +2,65 @@
 #define SIMPLE_VECTORS_H
 
 
-#include <math.h>
+#include <cmath>
+#include <iostream>
 
-
-template<typename T> class Vec2
+template<typename T> class Vec2_dbg
 {
 	T x;
 	T y;
+	T len;
+	
 
 public:
 	typedef T data_type;
+	
+	/* basic interfaces ... */
+	T X() const { return x; };
+	T Y() const { return y; };
+	T length() const { return len; }
+#ifdef DBG_SIMPLE_VECTORS
+	void TRACE() const
+	{
+		std::cout << "TRACING VECTOR " << this <<": X=" << x << ", Y=" << y << ", LEN="
+			<< len << std::endl;
+	}
+#endif
+
+	/* constructors ... */
+	Vec2_dbg(): x(0), y(0), len(0) {}
+	Vec2_dbg(T ix, T iy): x(ix), y(iy)
+	{
+		len = static_cast<T>( sqrt( x*x + y*y) );
+	}
+
+	/* operators and operations ... */
+	Vec2_dbg<T>& operator+(const Vec2_dbg<T> &inp) const
+	{
+		return *new Vec2_dbg<T>(this->x + inp.X(), this->y + inp.Y());
+	}
+	Vec2_dbg<T>& operator-() const
+	{
+		return *new Vec2_dbg<T>(-(this->x), -(this->y));
+	}
+	Vec2_dbg<T>& operator-(const Vec2_dbg<T> &inp) const
+	{
+		*this + (-inp);
+	}
+	Vec2_dbg<T>& normalize()
+	{
+		x /= len;
+		y /= len;
+		len = 1.0f;
+		return *this;
+	}
+	Vec2_dbg<T>& operator+=(const Vec2_dbg<T> &inp)
+	{
+		this->x += inp.X();
+		this->y += inp.Y();
+		this->len = sqrt(x*x + y*y);
+		return *this;
+	}
 };
 
 template<typename T> class Vec3
